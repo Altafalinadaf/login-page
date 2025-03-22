a09854a26cb975a78ee405283059002dda9fae67
altafalinadaf <altafalinadaf@gmail.com>
2025-03-22 15:49:17 +0530

    added comment

--- fetch_data/app.py
+++ fetch_data/app.py
@@ -10,6 +10,7 @@ def home():
 # Route to serve a simple message
 @app.route('/api/message', methods=['GET'])
 def get_message():
+    # this is the comment line
     return "Hello from the back-end! This data was fetched when you clicked the button."
 
 if __name__ == '__main__':
